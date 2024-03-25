81923e6e632cc774e7d4988c77aaf1531c5a9e8d
Andree Amaro <aamaro@skytouchtechnology.com>
2024-03-24 18:28:57 -0600

    Initial configuration for project barkmatch

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import SimpleBottomNavigation from './components/bottom-navigation/SimpleBottomNavigation'
+import DogCard from './components/profile-card/DogCard'
+
+export default function App () {
+  return (
+    <main>
+      <h1>Welcome to Barkmatch</h1>
+    </main>
+  )
+}
